c25473e9f4c70e908ed51261ae6113536ac62969
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-01-18 19:56:12 +0100

    varios cambios

--- producto.php
+++ producto.php
@@ -2,7 +2,7 @@
     error_reporting(E_ALL);
     ini_set('display_errors', '0');
     include_once("funciones.php");
-    $base_url = "http://www.demomotorrad.com.mialias.net"; //ABEL!!
+    // $base_url = "http://www.demomotorrad.com.mialias.net"; //ABEL!!
 
 
     // Obtener el modelo de la moto desde la URL
